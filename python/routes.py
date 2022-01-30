81e3982282f6e8b0e2b85c5dc4099748280b9e32
iamchrsgllsp <chrisgillespie2001@goooglemail.com>
2022-01-30 23:55:40 +0000

    Updating layout.html

--- python/routes.py
+++ python/routes.py
@@ -1,10 +1,12 @@
-from __main__ import app, render_template
+from __main__ import app, render_template, url_for
 
 
-@app.route('/', methods=['GET'])
+@app.route('/test', methods=['GET'])
 def test():
     return render_template('home.html')
 
-
+@app.route('/', methods=['GET'])
+def home():
+    return render_template('home.html')
 
 app.run(debug=True)
