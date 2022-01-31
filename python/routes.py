b28119971538a30a4ed39b899aa4bd76851d465d
iamchrsgllsp <chrisgillespie2001@goooglemail.com>
2022-01-31 00:59:18 +0000

    Added more routes

--- python/routes.py
+++ python/routes.py
@@ -5,10 +5,26 @@ from __main__ import app, render_template, url_for
 def test():
     return render_template('home.html')
 
+#Home Page
 @app.route('/', methods=['GET'])
 def home():
     user = "dave"
     data = ["hello","world","this is another test"]
     return render_template('home.html',user=user,data=data)
+
+#User Info
+@app.route('/signup', methods=['GET','POST'])
+def home():
+    user = "dave"
+    data = ["hello","world","this is another test"]
+    return render_template('home.html',user=user,data=data)
+
+@app.route('/login')
+def login():
+    return "Login screen"
+
+
+
+    
 
 app.run(debug=True)
